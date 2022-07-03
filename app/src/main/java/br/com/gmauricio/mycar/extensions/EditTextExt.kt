1f2386090de79acf733209f83a1d80a056ec4e3e
Guilherme M <gmauriciobr@gmail.com>
2022-07-03 02:21:18 -0300

    CRUD

--- app/src/main/java/br/com/gmauricio/mycar/extensions/EditTextExt.kt
+++ app/src/main/java/br/com/gmauricio/mycar/extensions/EditTextExt.kt
@@ -2,14 +2,22 @@ package br.com.gmauricio.mycar.extensions
 
 import android.widget.EditText
 
-    fun EditText.getLong() : Long {
-        return this.getString().toLong()
+fun EditText.getLong(): Long {
+    var value = this.getString()
+    if (value.isNotBlank()) {
+        return value.toLong()
     }
+    return 0
+}
 
-    fun EditText.getDouble() : Double {
-        return this.getString().toDouble()
+fun EditText.getDouble(): Double {
+    var value = this.getString()
+    if (value.isNotBlank()) {
+        return value.toDouble()
     }
+    return 0.0
+}
 
-    fun EditText.getString(): String {
-        return this.text.toString()
-    }
+fun EditText.getString(): String {
+    return this.text.toString()
+}
